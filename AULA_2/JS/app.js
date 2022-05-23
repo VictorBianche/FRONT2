e66552086fceda050504006be7836ded45df065a
Victor <victorbianche@hotmail.com>
2022-05-22 22:11:50 -0300

    updated and created new features

--- AULA_2/JS/app.js
+++ AULA_2/JS/app.js
@@ -5,34 +5,39 @@ let jogadores = [
     pontos:0,
     placarFinal:0
     },
-    {nome:'Maquina',
+    {nome:'Bot',
     pontos:0,
     placarFinal:0
     }
 ]
 
+function start(){
+    if(jogadores[0].pontos <= 3 || jogadores[1].pontos <= 3){ // valida o ponto dos jogadores
+        let escolhaUsuario = parseInt(prompt('[1] Pedra - [2] Papel - [3] Tesoura')); // Registra mão do Jogador
+        let escolhaScript = Math.floor(Math.random() * 3) + 1; // Gera mão do bot
+        jogar(escolhaUsuario,escolhaScript) //invoca a função do jogo
+    }else if(confirm("Continuar Jogando?")){
+        jogadores[0].pontos = 0; //Zera os pontos do Jogador
+        jogadores[1].pontos = 0; //Zera os pontos do Bot
+        start()
+    }
+}
+
 function jogar(escolhaUsuario,escolhaScript){
-    if(escolhaScript == 1 && escolhaUsuario == 3 ||escolhaScript == 3 && escolhaUsuario == 2||escolhaScript == 2 && escolhaUsuario == 1){
+    if(escolhaScript == 1 && escolhaUsuario == 3 ||escolhaScript == 3 && escolhaUsuario == 2||escolhaScript == 2 && escolhaUsuario == 1){ //Opções validas para vitoria do Bot
             alert('Vitoria do Bot')
             jogadores[1].pontos++
-        }else if(escolhaScript == escolhaUsuario){
+        }else if(escolhaScript == escolhaUsuario){ //Opções de Empate
             alert("Empate")
-        }else if(escolhaUsuario < 1 || escolhaUsuario > 3){
+        }else if(escolhaUsuario < 1 || escolhaUsuario > 3){ // Validação de dados errados
             alert("Dado invalido")       
-         }else{
+         }else{ //Opções de Vitoria do Jogador
             alert(`Você Ganhou`)
             jogadores[0].pontos++
         }
     console.log("Mão Jogador: " + escolhaUsuario)
-    console.log("Mão bot: " + escolhaScript)
+    console.log("Mão Bot: " + escolhaScript)
+    start()   
 };
 
-while(jogadores[0].pontos < 3||jogadores[1].pontos < 3){       
-    let escolhaUsuario = parseInt(prompt('[1] Pedra - [2] Papel - [3] Tesoura'));
-    let escolhaScript = Math.floor(Math.random() * 3) + 1;
-    jogar(escolhaUsuario,escolhaScript)
-    
-    console.log("Placar Jogador: " + jogadores[0].pontos)
-    console.log("Placar Bot: " + jogadores[1].pontos)
-}
-
+start()
